cfe4c5ebf24cbb3f3b155ccd9c5865f0276d53bf
Sherwin <sherwin.0014@gmail.com>
2017-01-24 23:56:13 +0800

    add games logic

--- app/src/main/java/com/webteq/guesstheinstruments/activities/GameActivity.java
+++ app/src/main/java/com/webteq/guesstheinstruments/activities/GameActivity.java
@@ -1,8 +1,11 @@
 package com.webteq.guesstheinstruments.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -18,7 +21,8 @@ public class GameActivity extends BaseActivity {
     private ArrayList<Integer> list;
     private int questionSize=0;
     private ImageView imgThumb;
-    private Button btnA,btnB,btnC,btnD;
+    private Button btnA,btnB,btnC,btnD,btnNext;
+    private GameModels model;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +35,32 @@ public class GameActivity extends BaseActivity {
         btnB = (Button) findViewById(R.id.choiceTwo);
         btnC = (Button) findViewById(R.id.choiceThree);
         btnD = (Button) findViewById(R.id.choiceFour);
+        btnNext = (Button) findViewById(R.id.next);
+
 
         loadQuestions();
         questionSize = gameModels.size();
-        list = new ArrayList<>(questionSize);
+        Log.d("c",questionSize+"");
 
-        GameModels model = gameModels.get(randomInt());
+        model = gameModels.get(randomInt());
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if(((Button)view).getText() == "Done"){
+                    Toast.makeText(getApplication(),"DONE!!",Toast.LENGTH_SHORT).show();
+                }else{
+                    model = gameModels.get(randomInt());
+                    loadData();
+                }
+            }
+        });
+
+        loadData();
+
+
+    }
+    private void loadData(){
         Glide.with(getBaseContext())
                 .load(model.getImage())
                 .error(R.drawable.placeholder)
@@ -56,17 +79,27 @@ public class GameActivity extends BaseActivity {
         gameModels.add(new GameModels(R.drawable.bagpipe_clipart,R.raw.bagpipes_sound,"Banjo","Bagpipes","Clarinet","Keyboard","Bagpipes"));
         gameModels.add(new GameModels(R.drawable.banjo_clipart,R.raw.banjo_sound,"Banjo","Bagpipes","Clarinet","Keyboard","Banjo"));
         gameModels.add(new GameModels(R.drawable.bass_clipart,R.raw.bass_drum_sound,"Bagpipes","Bass Drum","Keyboard","Clarinet","Bass Drum"));
-
+        gameModels.add(new GameModels(R.drawable.bagpipe_clipart,R.raw.bagpipes_sound,"Banjo","Bagpipes","Clarinet","Keyboard","Bagpipes"));
+        gameModels.add(new GameModels(R.drawable.banjo_clipart,R.raw.banjo_sound,"Banjo","Bagpipes","Clarinet","Keyboard","Banjo"));
+        list = new ArrayList<>(gameModels.size());
+        for(int i = 1; i <=gameModels.size(); i++) {
+            list.add(i);
+        }
     }
 
     private int randomInt(){
-        for(int i = 1; i <= questionSize; i++) {
-            list.add(i);
-        }
+        int index;
 
+
+        Log.d("pos",list.size() + "");
         Random rand = new Random();
-        int index = rand.nextInt(list.size());
+        index = rand.nextInt(list.size());
         list.remove(index);
+
+        if (list.size() < 1)
+        {
+            btnNext.setText("Done");
+        }
         return index;
     }
 }
